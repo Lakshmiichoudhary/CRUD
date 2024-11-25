5ed0df1f84b3ac6c05cdbf770d93494d05800cb1
lakshmiichoudhary <laxmi2252001@gmail.com>
2024-11-25 21:07:40 +0530

    Frontend and Backend setup

---
+++ frontend/src/components/ReadRecord.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReadRecord = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ReadRecord
---
+++ frontend/src/components/UpdateRecord.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateRecord = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UpdateRecord
